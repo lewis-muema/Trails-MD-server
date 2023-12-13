74007d832cd76b8186d3f234fe6be1823b5313f6
lewis-muema <lewis@sendy.co.ke>
2023-12-13 13:49:38 +0300

    Add update and delete endpoints

--- src/routes/trackRoutes.js
+++ src/routes/trackRoutes.js
@@ -2,6 +2,7 @@
 /* eslint-disable no-underscore-dangle */
 const express = require('express');
 const mongoose = require('mongoose');
+
 const requireAuth = require('../middlewares/requireAuth');
 
 const Track = mongoose.model('Track');
@@ -11,12 +12,14 @@ const router = express.Router();
 // Makes sure all routes in this file will only work if youre logged in
 router.use(requireAuth);
 
+// Getting all tracks
 router.get('/tracks', async (req, res) => {
   const tracks = await Track.find({ userId: req.user._id });
   const filteredTracks = [];
   tracks.forEach((track) => {
     filteredTracks.push({
       name: track.name,
+      id: track._id,
       locations: track.locations,
     });
   });
@@ -27,6 +30,28 @@ router.get('/tracks', async (req, res) => {
   }
 });
 
+// Getting a single track
+router.get('/tracks/:id', async (req, res) => {
+  if (mongoose.Types.ObjectId.isValid(req.params.id)) {
+    const id = new mongoose.Types.ObjectId(req.params.id);
+    try {
+      const track = await Track.findOne({ _id: id });
+      if (track) {
+        res.status(200).send({
+          name: track.name, id: track._id, locations: track.locations, message: 'Track found successfully',
+        });
+      } else {
+        res.status(200).send({ message: 'This trail cannot be found' });
+      }
+    } catch (err) {
+      res.status(400).send({ message: 'Something went wrong, Please try again later' });
+    }
+  } else {
+    res.status(400).send({ message: 'This trail cannot be found' });
+  }
+});
+
+// Adding a new track
 router.post('/tracks', async (req, res) => {
   const { name, locations } = req.body;
 
@@ -37,9 +62,55 @@ router.post('/tracks', async (req, res) => {
   try {
     const track = new Track({ name, locations, userId: req.user._id });
     await track.save();
-    res.status(200).send({ name: track._doc.name, locations: track._doc.locations, message: 'Track created successfully' });
+    res.status(200).send({
+      name: track._doc.name, id: track._doc._id, locations: track._doc.locations, message: 'Track created successfully',
+    });
   } catch (err) {
     res.status(401).status({ message: 'Sorry, Could not create a track. Please try again later' });
+  }
+});
+
+// Updating a single track
+router.put('/tracks/:id', async (req, res) => {
+  if (mongoose.Types.ObjectId.isValid(req.params.id)) {
+    const id = new mongoose.Types.ObjectId(req.params.id);
+    try {
+      const track = await Track.findOneAndUpdate({ _id: id }, req.body, {
+        returnOriginal: false,
+      });
+      if (track) {
+        res.status(200).send({
+          name: track.name, id: track._id, locations: track.locations, message: 'Track updated successfully',
+        });
+      } else {
+        res.status(200).send({ message: 'This trail cannot be found' });
+      }
+    } catch (err) {
+      res.status(400).send({ message: 'Failed to update this track, Please try again later' });
+    }
+  } else {
+    res.status(400).send({ message: 'This trail cannot be found' });
+  }
+});
+
+// Deleting a single track
+router.delete('/tracks/:id', async (req, res) => {
+  if (mongoose.Types.ObjectId.isValid(req.params.id)) {
+    const id = new mongoose.Types.ObjectId(req.params.id);
+    try {
+      const track = await Track.findOneAndDelete({ _id: id });
+      if (track) {
+        res.status(200).send({
+          name: track.name, id: track._id, locations: track.locations, message: 'Track deleted successfully',
+        });
+      } else {
+        res.status(200).send({ message: 'This trail cannot be found' });
+      }
+    } catch (err) {
+      res.status(400).send({ message: 'Failed to delete this track, Please try again later' });
+    }
+  } else {
+    res.status(400).send({ message: 'This trail cannot be found' });
   }
 });
 
